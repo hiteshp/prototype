9b3fc4a9b9d8f0087b9fc1e297c09b0699f4b912
hitesh <patel.hitesh@live.in>
2013-03-30 20:52:16 +0530

    log4j code in main

--- java-standalone/src/main/java/com/App.java
+++ java-standalone/src/main/java/com/App.java
@@ -1,5 +1,8 @@
 package com;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 /**
 *
  */
@@ -7,6 +10,10 @@ public class App
 {
     public static void main( String[] args )
     {
+        Logger logger = Logger.getLogger(App.class);
+        PropertyConfigurator.configure(App.class.getClassLoader().getResource("log4j.properties"));
+        logger.info("LOG INFO: App is initialized");
+
         System.out.println( "Hello World!" );
     }
 }
